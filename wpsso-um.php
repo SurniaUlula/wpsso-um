7fde96c3e64d385d70fa407bf8aeee89dece7fbf
JS Morisset <jsm@surniaulula.com>
2015-12-09 08:42:50 -0700

    v1.2.1

--- wpsso-um.php
+++ wpsso-um.php
@@ -11,8 +11,8 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to provide updates for the WordPress Social Sharing Optimization (WPSSO) Pro plugin and its Pro extensions.
  * Requires At Least: 3.1
- * Tested Up To: 4.3.1
- * Version: 1.2.0
+ * Tested Up To: 4.4
+ * Version: 1.2.1
  * 
  * Copyright 2015 - Jean-Sebastien Morisset - http://surniaulula.com/
  */
@@ -33,7 +33,7 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 
 		private static $wpsso_short = 'WPSSO';
 		private static $wpsso_name = 'WordPress Social Sharing Optimization (WPSSO)';
-		private static $wpsso_min_version = '3.15.0';
+		private static $wpsso_min_version = '3.18.0';
 		private static $wpsso_has_min_ver = true;
 
 		public static function &get_instance() {
