7420d78557f370ecd8e854f63cf5dcfc64671776
JS Morisset <jsm@surniaulula.com>
2016-04-25 16:13:06 -0400

    v1.5.2-dev3

--- wpsso-um.php
+++ wpsso-um.php
@@ -12,7 +12,7 @@
  * Description: WPSSO extension to provide updates for the WordPress Social Sharing Optimization (WPSSO) Pro plugin and its Pro extensions.
  * Requires At Least: 3.1
  * Tested Up To: 4.5
- * Version: 1.5.2-dev1
+ * Version: 1.5.2-dev3
  * 
  * Version Numbers: {major}.{minor}.{bugfix}-{stage}{level}
  *
@@ -42,7 +42,7 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 		private static $text_domain = 'wpsso-um';
 		private static $req_short = 'WPSSO';
 		private static $req_name = 'WordPress Social Sharing Optimization (WPSSO)';
-		private static $req_min_version = '3.29.7-dev2';
+		private static $req_min_version = '3.29.7-dev3';
 		private static $req_has_min_ver = true;
 
 		public static function &get_instance() {
