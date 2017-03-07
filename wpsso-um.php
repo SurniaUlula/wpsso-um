6d5c321458e0954efa1f197c2addb799549dd848
JS Morisset <jsm@surniaulula.com>
2017-03-06 18:53:15 -0800

    v1.5.17-1

--- wpsso-um.php
+++ wpsso-um.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to provide updates for the WordPress Social Sharing Optimization (WPSSO) Pro plugin and its Pro extensions.
  * Requires At Least: 3.8
- * Tested Up To: 4.7.2
+ * Tested Up To: 4.7.3
  * Version: 1.5.17-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
