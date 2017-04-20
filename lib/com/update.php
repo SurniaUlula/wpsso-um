0fcde17c0692aa9bdd40b3f1a7d926543ecb5b09
JS Morisset <jsm@surniaulula.com>
2017-04-20 12:45:48 +0200

    v1.6.2-b.1

--- lib/com/update.php
+++ lib/com/update.php
@@ -121,11 +121,11 @@ if ( ! class_exists( 'SucomUpdate' ) ) {
 				}
 			}
 
-			if ( is_admin() && $dev_selected ) {
+			if ( $dev_selected && $this->p->notice->is_admin_pre_notices() ) {
 				$warn_dis_key = 'non-stable-update-version-filters-selected';
 				$this->p->notice->warn( sprintf( __( 'Please note that one or more non-stable / development %s have been selected.',
 					$this->text_domain ), $this->p->util->get_admin_url( 'um-general', _x( 'Update Version Filters', 'metabox title', 'wpsso-um' ) ) ),
-						true, $warn_dis_key, MONTH_IN_SECONDS, true );	// $silent = true
+						true, $warn_dis_key, MONTH_IN_SECONDS * 3, true );	// $silent = true
 			}
 		}
 
