9b88a5c2029c3706bd41066dd4bde5a621b085d5
JS Morisset <jsm@surniaulula.com>
2017-04-22 08:42:40 +0200

    v1.6.2-rc.2

--- lib/config.php
+++ lib/config.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoum' => array(
-					'version' => '1.6.2-rc.1',		// plugin version
+					'version' => '1.6.2-rc.2',		// plugin version
 					'opt_version' => '3',		// increment when changing default options
 					'short' => 'WPSSO UM',		// short plugin name
 					'name' => 'WPSSO Update Manager (WPSSO UM)',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
 						'min_version' => '3.11.0',
-						'rec_version' => '3.41.0-rc.1',
+						'rec_version' => '3.41.0-rc.2',
 					),
 					'img' => array(
 						'icons' => array(
