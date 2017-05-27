6567036a7ddd56ed865824ce632c991858c2520f
JS Morisset <jsm@surniaulula.com>
2017-05-27 15:08:52 +0200

    v1.6.4-dev.3

--- lib/config.php
+++ lib/config.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoum' => array(
-					'version' => '1.6.4-dev.2',		// plugin version
+					'version' => '1.6.4-dev.3',		// plugin version
 					'opt_version' => '3',		// increment when changing default options
 					'short' => 'WPSSO UM',		// short plugin name
 					'name' => 'WPSSO Update Manager',
