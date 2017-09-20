918f80959124b1c93f1ae3bb5166d5f5ae895d6e
JS Morisset <jsm@surniaulula.com>
2017-09-20 09:22:59 -0400

    v1.6.6

--- wpsso-um.php
+++ wpsso-um.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to provide updates for the WPSSO Pro plugin and its Pro extensions.
  * Requires At Least: 3.7
- * Tested Up To: 4.8.1
+ * Tested Up To: 4.8.2
  * Requires PHP: 5.3
  * Version: 1.6.6
  * 
