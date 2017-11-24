0aee3c10dfa780e4c83df2829f54c3b3921247dd
JS Morisset <jsm@surniaulula.com>
2017-11-24 07:24:38 -0600

    v1.8.0-rc.1

--- lib/com-gettext-extract.php
+++ lib/com-gettext-extract.php
@@ -1,11 +1,11 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
 // extract of translatable text strings from common library classes
-__( 'Failed saving retrieved update information for %s.', wpsso-um );
-__( 'Please note that one or more non-stable / development %s have been selected.', wpsso-um );
-__( 'The %1$s plugin (%2$s) version number is missing from the WordPress plugins array.', wpsso-um );
-__( 'Update error from the WordPress wp_remote_get() function: %s', wpsso-um );
-__( 'Update information for %s has been retrieved and saved.', wpsso-um );
-__( 'WordPress function %s is missing and required.', wpsso-um );
-__( 'WordPress library file %s is missing and required.', wpsso-um );
-_x( 'Update Version Filters', 'metabox title', wpsso-um );
+__( 'Failed saving retrieved update information for %s.', 'wpsso-um' );
+__( 'Please note that one or more non-stable / development %s have been selected.', 'wpsso-um' );
+__( 'The %1$s plugin (%2$s) version number is missing from the WordPress plugins array.', 'wpsso-um' );
+__( 'Update error from the WordPress wp_remote_get() function: %s', 'wpsso-um' );
+__( 'Update information for %s has been retrieved and saved.', 'wpsso-um' );
+__( 'WordPress function %s is missing and required.', 'wpsso-um' );
+__( 'WordPress library file %s is missing and required.', 'wpsso-um' );
+_x( 'Update Version Filters', 'metabox title', 'wpsso-um' );
 ?>
