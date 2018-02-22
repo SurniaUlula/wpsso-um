2e71537fc831970b84971c8fc268494d53f6fda3
JS Morisset <jsm@surniaulula.com>
2018-02-22 07:39:44 -0600

    v1.9.0-rc.1

--- wpsso-um.php
+++ wpsso-um.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.4
  * Requires At Least: 3.8
  * Tested Up To: 4.9.4
- * Version: 1.9.0-b.1
+ * Version: 1.9.0-rc.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -161,9 +161,11 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 			}
 
 			$info = WpssoUmConfig::$cf['plugin']['wpssoum'];
+
 			$this->check_hours = $this->get_update_check_hours();
+
 			$this->filters = new WpssoUmFilters( $this->p );
-			$this->update = new SucomUpdate( $this->p, $this->check_hours, $info['text_domain'] );
+			$this->update  = new SucomUpdate( $this->p, $this->check_hours, $info['text_domain'] );
 		}
 
 		public function wpsso_init_plugin() {
@@ -177,23 +179,51 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 				return;	// stop here
 			}
 
-			if ( is_admin() ) {
+			$cache_md5_pre = $this->p->lca.'_';
+			$cache_salt = 'WpssoUm::cron_check';
+			$cache_id = $cache_md5_pre.md5( $cache_salt );
+
+			/**
+			 * Check if the WordPress cron is operating correctly.
+			 * Run on the admin back-end, and only once per day on the front-end.
+			 * If the transient does not exist, or has expired, then get_transient() will return false.
+			 */
+			if ( is_admin() || ! get_transient( $cache_id ) ) {
+
 				foreach ( $this->p->cf['plugin'] as $ext => $info ) {
-					if ( ! SucomUpdate::is_installed( $ext ) ) {				// plugin must be installed for updates
+
+					/**
+					 * The plugin must be installed to check for updates.
+					 */
+					if ( ! SucomUpdate::is_installed( $ext ) ) {
 						continue;
 					}
-					$last_utime = $this->update->get_umsg( $ext, 'time' );			// get the last update check
-					$next_utime = $last_utime + ( $this->check_hours * HOUR_IN_SECONDS );	// get the next scheduled check
-					if ( empty( $last_utime ) || $next_utime + DAY_IN_SECONDS < time() ) {	// check if more than 1 day overdue
+
+					$now_time = time();
+
+					$last_check_time = $this->update->get_umsg( $ext, 'time' ); // get the last update check timestamp
+					$last_plus_week  = $last_check_time + WEEK_IN_SECONDS;
+
+					$next_sched_time = $last_check_time + ( $this->check_hours * HOUR_IN_SECONDS ); // estimate the next scheduled check
+					$next_plus_day   = $next_sched_time + DAY_IN_SECONDS;
+
+					/**
+					 * Force an update check if no last time, more than 1 day overdue, or more than 1 week ago.
+					 */
+					if ( empty( $last_check_time ) || $next_plus_day < $now_time || $last_plus_week < $now_time ) {
+
 						if ( $this->p->debug->enabled ) {
 							$dismiss_key = __FUNCTION__ . '_' . $ext . '_update_check';
 							$this->p->debug->log( 'requesting update check for ' . $ext );
 							$this->p->notice->inf( sprintf( __( 'Performing an update check for the %s plugin.',
 								'wpsso-um' ), $info['name'] ), true, $dismiss_key, true );
 						}
+
 						$this->update->check_ext_for_updates( $ext, true, false );	// $quiet = true, $use_cache = false
 					}
 				}
+
+				set_transient( $cache_id, 1, DAY_IN_SECONDS );
 			}
 		}
 
@@ -215,16 +245,23 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 			}
 		}
 
-		// minimum value is 12 hours for the constant, 24 hours otherwise
 		public function get_update_check_hours() {
-			if ( SucomUtil::get_const( 'WPSSOUM_CHECK_HOURS', 0 ) >= 12 ) {
-				return WPSSOUM_CHECK_HOURS;
-			} elseif ( isset( $this->p->options['update_check_hours'] ) &&
-				$this->p->options['update_check_hours'] >= 24 ) {
-				return $this->p->options['update_check_hours'];
-			} else {
-				return 24;	// default value
+
+			$check_hours = 24;
+			$const_hours = SucomUtil::get_const( 'WPSSOUM_CHECK_HOURS', null );	// return null if not defined
+			$opt_hours = isset( $this->p->options['update_check_hours'] ) ? $this->p->options['update_check_hours'] : 24;
+
+			if ( $const_hours !== null ) {
+				$check_hours = $const_hours >= 12 ? WPSSOUM_CHECK_HOURS : 12;
+			} elseif ( $opt_hours >= 24 ) {
+				$check_hours = $opt_hours;
 			}
+
+			if ( $check_hours > WEEK_IN_SECONDS ) {	// check at least once a week
+				$check_hours = WEEK_IN_SECONDS;
+			}
+
+			return $check_hours;
 		}
 	}
 
