2e6446a80c51061e035c7b49ccaa07c7729ae5c4
JS Morisset <jsm@surniaulula.com>
2018-05-16 17:17:02 -0400

    v1.9.3

--- wpsso-um.php
+++ wpsso-um.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to provide updates for the WPSSO Core Pro plugin and its Pro add-ons.
  * Requires PHP: 5.4
  * Requires At Least: 3.8
- * Tested Up To: 4.9.5
+ * Tested Up To: 4.9.6
  * Version: 1.9.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
