404f3331dc239b418f37738458653bf9cf0b0098
JS Morisset <jsm@surniaulula.com>
2018-10-12 07:14:59 -0600

    v1.12.2-dev.1

---
+++ lib/com/update-util-wp.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Copyright 2015-2018 Jean-Sebastien Morisset (https://wpsso.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'These aren\'t the droids you\'re looking for...' );
+}
+
+if ( ! class_exists( 'SucomUpdateUtilWP' ) ) {
+
+	class SucomUpdateUtilWP {
+
+		/**
+		 * Unfiltered version of home_url() from wordpress/wp-includes/link-template.php
+		 * Last synchronized with WordPress v4.8.2 on 2017/10/22.
+		 */
+		public static function raw_home_url( $path = '', $scheme = null ) {
+
+			return self::raw_get_home_url( null, $path, $scheme );
+		}
+
+		/**
+		 * Unfiltered version of get_home_url() from wordpress/wp-includes/link-template.php
+		 * Last synchronized with WordPress v4.8.2 on 2017/10/22.
+		 */
+		public static function raw_get_home_url( $blog_id = null, $path = '', $scheme = null ) {
+
+			global $pagenow;
+
+			if ( method_exists( 'SucomUtil', 'protect_filter_value' ) ) {
+				SucomUtil::protect_filter_value( 'pre_option_home' );
+			}
+
+			if ( empty( $blog_id ) || ! is_multisite() ) {
+
+				$url = get_option( 'home' );
+
+			} else {
+
+				switch_to_blog( $blog_id );
+
+				$url = get_option( 'home' );
+
+				restore_current_blog();
+			}
+
+			if ( ! in_array( $scheme, array( 'http', 'https', 'relative' ) ) ) {
+
+				if ( is_ssl() && ! is_admin() && 'wp-login.php' !== $pagenow ) {
+
+					$scheme = 'https';
+				} else {
+					$scheme = parse_url( $url, PHP_URL_SCHEME );
+				}
+			}
+
+			$url = self::raw_set_url_scheme( $url, $scheme );
+
+			if ( $path && is_string( $path ) ) {
+				$url .= '/' . ltrim( $path, '/' );
+			}
+
+			return $url;
+		}
+
+		/**
+		 * Unfiltered version of set_url_scheme() from wordpress/wp-includes/link-template.php
+		 * Last synchronized with WordPress v4.8.2 on 2017/10/22.
+		 */
+		private static function raw_set_url_scheme( $url, $scheme = null ) {
+
+			if ( ! $scheme ) {
+				$scheme = is_ssl() ? 'https' : 'http';
+			} elseif ( $scheme === 'admin' || $scheme === 'login' || $scheme === 'login_post' || $scheme === 'rpc' ) {
+				$scheme = is_ssl() || force_ssl_admin() ? 'https' : 'http';
+			} elseif ( $scheme !== 'http' && $scheme !== 'https' && $scheme !== 'relative' ) {
+				$scheme = is_ssl() ? 'https' : 'http';
+			}
+
+			$url = trim( $url );
+
+			if ( substr( $url, 0, 2 ) === '//' ) {
+				$url = 'http:' . $url;
+			}
+
+			if ( 'relative' === $scheme ) {
+
+				$url = ltrim( preg_replace( '#^\w+://[^/]*#', '', $url ) );
+
+				if ( $url !== '' && $url[0] === '/' ) {
+					$url = '/' . ltrim( $url, "/ \t\n\r\0\x0B" );
+				}
+
+			} else {
+				$url = preg_replace( '#^\w+://#', $scheme . '://', $url );
+			}
+
+			return $url;
+		}
+	}
+}
