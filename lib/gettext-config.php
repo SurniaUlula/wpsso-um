43a4d55518c6b89478505048ea93805fe5239fb3
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:50:53 -0600

    v1.14.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,4 +1,8 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
+_x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'WPSSO Core add-on to provide updates for the WPSSO Core Pro plugin and its Pro add-ons.', 'plugin description', 'wpsso-um' );
