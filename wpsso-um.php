df1c0eda11e4b57356f64b3c50d2e27e67302fe8
JS Morisset <jsm@surniaulula.com>
2019-03-22 04:43:04 -0600

    v1.18.0

--- wpsso-um.php
+++ wpsso-um.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on provides updates for the WPSSO Core Pro plugin and its Pro add-ons.
- * Requires PHP: 5.5
+ * Requires PHP: 5.6
  * Requires At Least: 3.8
  * Tested Up To: 5.1
  * Version: 1.18.0
