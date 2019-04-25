375774af749a0494a180ed1aa3b7608034109695
JS Morisset <jsm@surniaulula.com>
2019-04-25 16:25:40 -0700

    v1.19.3-dev.5

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoum' => array(			// Plugin acronym.
-					'version'     => '1.19.3-dev.4',	// Plugin version.
+					'version'     => '1.19.3-dev.5',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
@@ -30,7 +30,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
 						'min_version' => '4.4.0',	// Released on 2018/06/07.
-						'rec_version' => '4.29.1-dev.4',
+						'rec_version' => '4.29.1-dev.5',
 					),
 					'assets' => array(
 						'icons' => array(
