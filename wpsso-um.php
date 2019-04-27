dea54b7658585f1ab71eb43594c9eab6d9929f72
JS Morisset <jsm@surniaulula.com>
2019-04-27 16:03:29 -0700

    v1.19.3

--- wpsso-um.php
+++ wpsso-um.php
@@ -12,8 +12,8 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on provides updates for the WPSSO Core Pro plugin and its Pro add-ons.
  * Requires At Least: 3.8
- * Tested Up To: 5.1.1
- * Version: 1.19.3-rc.1
+ * Tested Up To: 5.2
+ * Version: 1.19.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
