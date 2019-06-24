1d4e0e51a617cbde054cbc027b969112214ed692
JS Morisset <jsm@surniaulula.com>
2019-06-24 12:15:53 -0700

    v2.0.0

--- lib/config.php
+++ lib/config.php
@@ -20,7 +20,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
-					'desc'        => 'WPSSO Core add-on provides updates for the WPSSO Core Pro plugin and its complementary Pro add-ons.',
+					'desc'        => 'WPSSO Core add-on provides updates for the WPSSO Core Premium plugin and its complementary Premium add-ons.',
 					'slug'        => 'wpsso-um',
 					'base'        => 'wpsso-um/wpsso-um.php',
 					'update_auth' => '',
--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -5,4 +5,4 @@
  */
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
-_x( 'WPSSO Core add-on provides updates for the WPSSO Core Pro plugin and its complementary Pro add-ons.', 'plugin description', 'wpsso-um' );
+_x( 'WPSSO Core add-on provides updates for the WPSSO Core Premium plugin and its complementary Premium add-ons.', 'plugin description', 'wpsso-um' );
