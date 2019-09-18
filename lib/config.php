5d0499c79db321b1f56a3bc751f5d53ac4e2fcaf
JS Morisset <jsm@surniaulula.com>
2019-09-17 20:03:54 -0700

    v2.2.0

--- lib/config.php
+++ lib/config.php
@@ -30,7 +30,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
 						'min_version' => '4.8.0',	// Released on 2018/07/22.
-						'rec_version' => '6.1.0',
+						'rec_version' => '6.3.1',
 					),
 					'assets' => array(
 						'icons' => array(
