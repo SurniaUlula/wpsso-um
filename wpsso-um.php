14e6eec1e066ce7cc4851a8d8e7cc7b892f256e8
JS Morisset <jsm@surniaulula.com>
2020-03-04 10:42:34 -0800

    v2.6.5

--- wpsso-um.php
+++ wpsso-um.php
@@ -13,7 +13,7 @@
  * Description: Update manager for the WPSSO Core Premium plugin and its complementary Premium add-ons.
  * Requires PHP: 5.5
  * Requires At Least: 4.0
- * Tested Up To: 5.3.2
+ * Tested Up To: 5.4
  * Version: 2.6.5
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
