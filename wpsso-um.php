22cd11681341882e2ee4a332d933c96b93cf7f8d
JS Morisset <jsm@surniaulula.com>
2020-03-04 14:14:56 -0800

    v2.6.5

--- wpsso-um.php
+++ wpsso-um.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Update manager for the WPSSO Core Premium plugin and its complementary Premium add-ons.
- * Requires PHP: 5.5
+ * Requires PHP: 5.6
  * Requires At Least: 4.0
  * Tested Up To: 5.4
  * Version: 2.6.5
