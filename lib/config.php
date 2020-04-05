311af0f6819af4aee8b8163eb1465a84645ed149
JS Morisset <jsm@surniaulula.com>
2020-04-05 12:09:28 -0700

    v2.8.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoum' => array(			// Plugin acronym.
-					'version'     => '2.8.0-b.1',	// Plugin version.
+					'version'     => '2.8.0-rc.1',	// Plugin version.
 					'opt_version' => '4',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
