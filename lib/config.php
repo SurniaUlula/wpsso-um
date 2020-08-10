f1374594a82d04b707782b5fe15685a0f0473ce8
JS Morisset <jsm@surniaulula.com>
2020-08-10 12:14:35 -0700

    v3.1.0-dev.10

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoum' => array(			// Plugin acronym.
-					'version'     => '3.1.0-dev.9',	// Plugin version.
+					'version'     => '3.1.0-dev.10',	// Plugin version.
 					'opt_version' => '5',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
