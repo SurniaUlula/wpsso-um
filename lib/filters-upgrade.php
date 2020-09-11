005fca58eb6473ce738c8998430cdfabe05f1201
JS Morisset <jsm@surniaulula.com>
2020-09-11 09:20:43 -0700

    v3.2.0-dev.1

---
+++ lib/filters-upgrade.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Copyright 2014-2020 Jean-Sebastien Morisset (https://wpsso.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+
+	die( 'These aren\'t the droids you\'re looking for.' );
+}
+
+if ( ! class_exists( 'WpssoUmFiltersUpgrade' ) ) {
+
+	class WpssoUmFiltersUpgrade {
+
+		private $p;
+
+		/**
+		 * Instantiated by WpssoUmFilters->__construct().
+		 */
+		public function __construct( &$plugin ) {
+
+			static $do_once = null;
+
+			if ( true === $do_once ) {
+
+				return;	// Stop here.
+			}
+
+			$do_once = true;
+
+			$this->p =& $plugin;
+
+			if ( $this->p->debug->enabled ) {
+
+				$this->p->debug->mark();
+			}
+
+			$this->p->util->add_plugin_filters( $this, array(
+				'rename_options_keys'    => 1,
+			) );
+		}
+
+		public function filter_rename_options_keys( $options_keys ) {
+
+			if ( $this->p->debug->enabled ) {
+
+				$this->p->debug->mark();
+			}
+
+			$options_keys[ 'wpssoum' ] = array(
+				6 => array(
+					'update_check_hours' => '',
+				),
+			);
+
+			return $options_keys;
+		}
+	}
+}
