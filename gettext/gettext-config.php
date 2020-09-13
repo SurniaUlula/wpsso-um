d74bc56299cce93a3fa1be9d71761d95330bb3e6
JS Morisset <jsm@surniaulula.com>
2020-09-13 13:08:01 -0700

    v3.2.0

---
+++ gettext/gettext-config.php
@@ -0,0 +1,8 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'Update Manager', 'lib file description', 'wpsso-um' );
+_x( 'Update Manager', 'lib file description', 'wpsso-um' );
+_x( 'Update Manager for the WPSSO Core Premium Plugin and its Premium Complementary Add-ons.', 'plugin description', 'wpsso-um' );
