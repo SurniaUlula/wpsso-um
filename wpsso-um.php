a7cbc7ead7ae4b3637bd9573ee90110ff0085888
JS Morisset <jsm@surniaulula.com>
2021-02-25 09:22:34 -0800

    v4.4.1

--- wpsso-um.php
+++ wpsso-um.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.0
  * Requires At Least: 4.5
  * Tested Up To: 5.6.2
- * Version: 4.4.0
+ * Version: 4.4.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
